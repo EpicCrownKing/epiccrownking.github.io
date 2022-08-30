157bc359e191ff07b9daddc9979379347b115dd8
lukemb152 <epiccrownking@gmail.com>
2022-08-29 20:45:52 -0400

    Added files

--- ModHelper/common.js
+++
@@ -1,5 +0,0 @@
-var sources='using Microsoft.Xna.Framework; \
-using Terraria; \
-using Terraria.ID; \
-using Terraria.ModLoader; \
-using static Terraria.ModLoader.ModContent;';
---
+++ common.js
@@ -0,0 +1,5 @@
+var sources='using Microsoft.Xna.Framework; \
+using Terraria; \
+using Terraria.ID; \
+using Terraria.ModLoader; \
+using static Terraria.ModLoader.ModContent;';
